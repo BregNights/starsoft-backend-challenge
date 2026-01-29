58076b0356df7a92596652a2093f74da8785e007
BregNights <daniel_baumann567@hotmail.com>
2026-01-29 15:53:21 -0300

    Create create-user.ts

---
+++ src/domain/cine/application/use-cases/create-user.ts
@@ -0,0 +1,32 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { User, UsersRepository } from '../repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface CreateAccountUseCaseRequest {
+  name: string
+  email: string
+}
+
+type CreateAccountUseCaseResponse = Either<
+  UserAlreadyExistsError,
+  {
+    user: User
+  }
+>
+
+@Injectable()
+export class CreateAccountUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+  async execute({
+    name,
+    email,
+  }: CreateAccountUseCaseRequest): Promise<CreateAccountUseCaseResponse> {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+    if (userWithSameEmail) return left(new UserAlreadyExistsError(email))
+
+    const user = await this.usersRepository.create({ name, email })
+
+    return right({ user })
+  }
+}
