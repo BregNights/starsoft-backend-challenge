f38c465dadc801ca475b688ffbb70a1008607e41
BregNights <daniel_baumann567@hotmail.com>
2026-01-29 15:51:09 -0300

    Create create-user.spec.ts

---
+++ src/domain/cine/application/use-cases/create-user.spec.ts
@@ -0,0 +1,21 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
+import { CreateAccountUseCase } from './create-user'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: CreateAccountUseCase
+
+describe('Create Courier', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new CreateAccountUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able register a new courier', async () => {
+    const result = await sut.execute({
+      name: 'example',
+      email: 'example@example.com',
+    })
+
+    expect(result.isRight()).toBe(true)
+  })
+})
