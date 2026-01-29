ec62e115e69e6b626c613fb90b9401bf212c09d0
BregNights <daniel_baumann567@hotmail.com>
2026-01-29 15:52:22 -0300

    Create vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+
+import tsConfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: "./src",
+    cache: false,
+  },
+  plugins: [
+    tsConfigPaths(),
+  ],
+})
