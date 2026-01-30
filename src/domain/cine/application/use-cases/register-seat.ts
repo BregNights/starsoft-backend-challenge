7d99f825a6c42a8bb685832bc89288c4a0ad43e4
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 12:56:46 -0300

    Create register-seat.ts

---
+++ src/domain/cine/application/use-cases/register-seat.ts
@@ -0,0 +1,44 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { SeatsRepository } from '../repositories/seat-respository'
+import { SessionsRepository } from '../repositories/session-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { SessionAlreadyExistsSeatsError } from './errors/session-already-exists-seats-error'
+
+interface RegisterSeatUseCaseRequest {
+  seatNumber: string
+  sessionId: string
+}
+
+type RegisterSeatUseCaseResponse = Either<
+  ResourceNotFoundError | SessionAlreadyExistsSeatsError,
+  null
+>
+
+@Injectable()
+export class RegisterSeatUseCase {
+  constructor(
+    private seatsRepository: SeatsRepository,
+    private sessionsRepository: SessionsRepository,
+  ) {}
+  async execute({
+    seatNumber,
+    sessionId,
+  }: RegisterSeatUseCaseRequest): Promise<RegisterSeatUseCaseResponse> {
+    console.log(sessionId)
+    const session = await this.sessionsRepository.findById(sessionId)
+    if (!session) return left(new ResourceNotFoundError())
+
+    const seatSession = await this.seatsRepository.findManySeatsBySessionId(session.id!)
+    console.log(seatSession)
+    // if (seatSession?.seatNumber)
+    //   return left(new SessionAlreadyExistsSeatsError())
+
+    await this.seatsRepository.create({
+      seatNumber,
+      sessionId,
+    })
+
+    return right(null)
+  }
+}
