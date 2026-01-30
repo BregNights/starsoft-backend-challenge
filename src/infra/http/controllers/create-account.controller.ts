9961f792209ef8b09f0a66b9e63b0fb83458464b
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 19:29:05 -0300

    Create create-account.controller.ts

---
+++ src/infra/http/controllers/create-account.controller.ts
@@ -0,0 +1,39 @@
+import { CreateAccountUseCase } from '@/domain/cine/application/use-cases/create-account'
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import z from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.email(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private usecase: CreateAccountUseCase) {}
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email } = body
+
+    const result = await this.usecase.execute({
+      name,
+      email,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+      throw new ConflictException(error.message)
+    }
+  }
+}
