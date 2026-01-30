b1c6e0b844bb101a22cc64c75ff276d0e1977a93
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 13:00:03 -0300

    add SessionsRepository, SeatsRepository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,10 @@
+import { SeatsRepository } from '@/domain/cine/application/repositories/seat-respository'
+import { SessionsRepository } from '@/domain/cine/application/repositories/session-repository'
 import { UsersRepository } from '@/domain/cine/application/repositories/users-repository'
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaSeatsRepository } from './prisma/repositories/prisma-seats-repository'
+import { PrismaSessionsRepository } from './prisma/repositories/prisma-sessions-repository'
 import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository'
 
 @Module({
@@ -11,7 +15,20 @@ import { PrismaUsersRepository } from './prisma/repositories/prisma-users-reposi
       provide: UsersRepository,
       useClass: PrismaUsersRepository,
     },
+    {
+      provide: SessionsRepository,
+      useClass: PrismaSessionsRepository,
+    },
+    {
+      provide: SeatsRepository,
+      useClass: PrismaSeatsRepository,
+    },
   ],
-  exports: [PrismaService, UsersRepository],
+  exports: [
+    PrismaService,
+    UsersRepository,
+    SessionsRepository,
+    SeatsRepository,
+  ],
 })
 export class DatabaseModule {}
