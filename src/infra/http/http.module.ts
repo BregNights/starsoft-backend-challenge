cffbe6ecdc11b155b55632cf10185807abd692db
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 19:30:11 -0300

    add FetchSeatBySessionController

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,12 @@
+import { CreateAccountUseCase } from '@/domain/cine/application/use-cases/create-account'
 import { CreateSessionUseCase } from '@/domain/cine/application/use-cases/create-session'
-import { CreateAccountUseCase } from '@/domain/cine/application/use-cases/create-user'
+import { FetchSeatBySessionUseCase } from '@/domain/cine/application/use-cases/fetch-seat-by-session'
 import { RegisterSeatUseCase } from '@/domain/cine/application/use-cases/register-seat'
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
 import { CreateSessionController } from './controllers/create-session.controller'
-import { CreateAccountController } from './controllers/create-user.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { FetchSeatBySessionController } from './controllers/fetch-seat-by-session.controller'
 import { RegisterSeatController } from './controllers/register-seat-controller'
 
 @Module({
@@ -13,7 +15,13 @@ import { RegisterSeatController } from './controllers/register-seat-controller'
     CreateAccountController,
     CreateSessionController,
     RegisterSeatController,
+    FetchSeatBySessionController,
   ],
-  providers: [CreateAccountUseCase, CreateSessionUseCase, RegisterSeatUseCase],
+  providers: [
+    CreateAccountUseCase,
+    CreateSessionUseCase,
+    RegisterSeatUseCase,
+    FetchSeatBySessionUseCase,
+  ],
 })
 export class HttpModule {}
