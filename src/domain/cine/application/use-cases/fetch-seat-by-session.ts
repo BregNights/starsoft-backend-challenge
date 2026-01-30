5e4405c8b9c98ca08f9f186b16e780d07dadc04e
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 19:28:59 -0300

    Create fetch-seat-by-session.ts

---
+++ src/domain/cine/application/use-cases/fetch-seat-by-session.ts
@@ -0,0 +1,32 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Seat, SeatsRepository } from '../repositories/seat-respository'
+import { SessionsRepository } from '../repositories/session-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface FetchSeatBySessionUseCaseRequest {
+  sessionId: string
+}
+
+type FetchSeatBySessionUseCaseResponse = Either<
+  ResourceNotFoundError,
+  { seat: Seat[] }
+>
+
+@Injectable()
+export class FetchSeatBySessionUseCase {
+  constructor(
+    private seatsRepository: SeatsRepository,
+    private sessionsRepository: SessionsRepository,
+  ) {}
+  async execute({
+    sessionId,
+  }: FetchSeatBySessionUseCaseRequest): Promise<FetchSeatBySessionUseCaseResponse> {
+    const session = await this.sessionsRepository.findById(sessionId)
+    if (!session) return left(new ResourceNotFoundError())
+
+    const seat = await this.seatsRepository.findManySeatsBySessionId(sessionId)
+
+    return right({ seat })
+  }
+}
