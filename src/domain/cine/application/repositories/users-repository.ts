64cf5612b24a9d8fd159a2bd1d440cb27a721a8a
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 12:55:57 -0300

    add id, createdAt on interface

--- src/domain/cine/application/repositories/users-repository.ts
+++ src/domain/cine/application/repositories/users-repository.ts
@@ -1,9 +1,11 @@
 export interface User {
+  id?: string
   name: string
   email: string
+  createdAt?: Date
 }
 
 export abstract class UsersRepository {
-  abstract create(user: User): Promise<User>
+  abstract create(user: User): Promise<void>
   abstract findByEmail(email: string): Promise<User | null>
 }
