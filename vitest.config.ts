a2b7f73f018e81ef17db68460e125e03463d6689
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 19:30:25 -0300

    add swc config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,17 @@
-
-import tsConfigPaths from "vite-tsconfig-paths"
-import { defineConfig } from "vitest/config"
+import swc from 'unplugin-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
-    root: "./src",
+    root: './src',
     cache: false,
   },
   plugins: [
     tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
   ],
 })
