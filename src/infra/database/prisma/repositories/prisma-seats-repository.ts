5ba5fa1791bca2ede8e18756235b25055053f848
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 12:56:55 -0300

    Create prisma-seats-repository.ts

---
+++ src/infra/database/prisma/repositories/prisma-seats-repository.ts
@@ -0,0 +1,38 @@
+import {
+  Seat,
+  SeatsRepository,
+} from '@/domain/cine/application/repositories/seat-respository'
+import { Injectable } from '@nestjs/common'
+import { SeatStatus } from 'generated/prisma/enums'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaSeatsRepository implements SeatsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(seat: Seat): Promise<void> {
+    await this.prisma.seat.create({
+      data: {
+        seatNumber: seat.seatNumber,
+        sessionId: seat.sessionId,
+        status: SeatStatus.AVAILABLE,
+      },
+    })
+  }
+
+  async findBySessionId(sessionId: string): Promise<Seat | null> {
+    const session = await this.prisma.seat.findFirst({
+      where: { sessionId: sessionId },
+    })
+
+    return session ?? null
+  }
+
+  async findManySeatsBySessionId(sessionId: string): Promise<Seat[]> {
+    const session = await this.prisma.seat.findMany({
+      where: { sessionId: sessionId },
+    })
+
+    return session ?? null
+  }
+}
