dfcbcf3c6d4af603a97e9a4c55a62c8652f8c1ae
BregNights <daniel_baumann567@hotmail.com>
2026-01-30 12:59:17 -0300

    add CreateSessionController and RegisterSeatController

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,19 @@
+import { CreateSessionUseCase } from '@/domain/cine/application/use-cases/create-session'
 import { CreateAccountUseCase } from '@/domain/cine/application/use-cases/create-user'
+import { RegisterSeatUseCase } from '@/domain/cine/application/use-cases/register-seat'
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
-import { CreateAccount } from './controllers/create-user.controller'
+import { CreateSessionController } from './controllers/create-session.controller'
+import { CreateAccountController } from './controllers/create-user.controller'
+import { RegisterSeatController } from './controllers/register-seat-controller'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateAccount],
-  providers: [CreateAccountUseCase],
+  controllers: [
+    CreateAccountController,
+    CreateSessionController,
+    RegisterSeatController,
+  ],
+  providers: [CreateAccountUseCase, CreateSessionUseCase, RegisterSeatUseCase],
 })
 export class HttpModule {}
