03211612f78e7f1934883deb205f00dac8edc862
BregNights <daniel_baumann567@hotmail.com>
2026-01-31 19:06:49 -0300

    add 2 new unit test

--- src/domain/cine/application/use-cases/create-session.spec.ts
+++ src/domain/cine/application/use-cases/create-session.spec.ts
@@ -1,5 +1,7 @@
 import { InMemorySessionsRepository } from 'test/repositories/in-memory-sessions-repository'
 import { CreateSessionUseCase } from './create-session'
+import { InvalidSessionPriceError } from './errors/invalid-session-price.error'
+import { SessionInPastError } from './errors/session-in-past.error'
 
 let inMemorySessionsRepository: InMemorySessionsRepository
 let sut: CreateSessionUseCase
@@ -22,5 +24,35 @@ describe('Create Session', () => {
     })
 
     expect(result.isRight()).toBe(true)
+  })
+
+  it('should not be able create a new session without price', async () => {
+    const startsAt = new Date()
+    startsAt.setMonth(startsAt.getMonth() + 1)
+
+    const result = await sut.execute({
+      movieTitle: 'Iron Man',
+      price: 0,
+      room: '1',
+      startsAt,
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidSessionPriceError)
+  })
+
+  it('should not be able create a new session in the past', async () => {
+    const startsAt = new Date()
+    startsAt.setMonth(startsAt.getMonth() - 1)
+
+    const result = await sut.execute({
+      movieTitle: 'Iron Man',
+      price: 28.99,
+      room: '1',
+      startsAt,
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(SessionInPastError)
   })
 })
