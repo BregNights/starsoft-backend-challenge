8e8d060451ca31d511cbd9f3e9b540a7b197af63
BregNights <daniel_baumann567@hotmail.com>
2026-01-31 19:09:11 -0300

    add CreateReservationController

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,13 @@
 import { CreateAccountUseCase } from '@/domain/cine/application/use-cases/create-account'
+import { CreateReservationUseCase } from '@/domain/cine/application/use-cases/create-reservation'
 import { CreateSessionUseCase } from '@/domain/cine/application/use-cases/create-session'
 import { FetchSeatBySessionUseCase } from '@/domain/cine/application/use-cases/fetch-seat-by-session'
 import { RegisterSeatUseCase } from '@/domain/cine/application/use-cases/register-seat'
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
-import { CreateSessionController } from './controllers/create-session.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateReservationController } from './controllers/create-reservation.controller'
+import { CreateSessionController } from './controllers/create-session.controller'
 import { FetchSeatBySessionController } from './controllers/fetch-seat-by-session.controller'
 import { RegisterSeatController } from './controllers/register-seat-controller'
 
@@ -16,12 +18,14 @@ import { RegisterSeatController } from './controllers/register-seat-controller'
     CreateSessionController,
     RegisterSeatController,
     FetchSeatBySessionController,
+    CreateReservationController,
   ],
   providers: [
     CreateAccountUseCase,
     CreateSessionUseCase,
     RegisterSeatUseCase,
     FetchSeatBySessionUseCase,
+    CreateReservationUseCase,
   ],
 })
 export class HttpModule {}
