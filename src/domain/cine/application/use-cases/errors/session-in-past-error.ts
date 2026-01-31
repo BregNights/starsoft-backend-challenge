5274c8fd7dbdc5aeb472ac07639212eec840b69d
BregNights <daniel_baumann567@hotmail.com>
2026-01-31 19:04:36 -0300

    Delete session-in-past-error.ts

--- src/domain/cine/application/use-cases/errors/session-in-past-error.ts
+++
@@ -1,7 +0,0 @@
-import { UseCaseError } from './use-case-error'
-
-export class SessionInPastError extends Error implements UseCaseError {
-  constructor() {
-    super('Session cannot be created in the past')
-  }
-}
