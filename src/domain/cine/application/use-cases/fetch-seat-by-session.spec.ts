b9d6f862bad5659c8bb14d35403b6a41db9f90b5
BregNights <daniel_baumann567@hotmail.com>
2026-01-31 19:07:27 -0300

    add new unit test

--- src/domain/cine/application/use-cases/fetch-seat-by-session.spec.ts
+++ src/domain/cine/application/use-cases/fetch-seat-by-session.spec.ts
@@ -1,5 +1,6 @@
 import { InMemorySeatsRepository } from 'test/repositories/in-memory-seats-repository'
 import { InMemorySessionsRepository } from 'test/repositories/in-memory-sessions-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
 import { FetchSeatBySessionUseCase } from './fetch-seat-by-session'
 
 let inMemorySeatsRepository: InMemorySeatsRepository
@@ -16,7 +17,7 @@ describe('Fetch Seats By Session', () => {
     )
 
     await inMemorySessionsRepository.create({
-      id: '2',
+      id: '1',
       movieTitle: 'Example',
       room: '1',
       price: 10,
@@ -28,19 +29,28 @@ describe('Fetch Seats By Session', () => {
         seatNumber: `A${i}`,
         status: 'AVAILABLE',
         sessionId: '1',
-        id: '2',
+        id: '1',
       })
     }
   })
 
   it('should be able to fetch seats', async () => {
     const result = await sut.execute({
-      sessionId: '2',
+      sessionId: '1',
     })
 
     expect(result.isRight()).toBe(true)
     if (result.isRight()) {
       expect(result.value.seat).toHaveLength(2)
     }
+  })
+
+  it('should not be able to fetch seats for a session that does not exist', async () => {
+    const result = await sut.execute({
+      sessionId: '2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
