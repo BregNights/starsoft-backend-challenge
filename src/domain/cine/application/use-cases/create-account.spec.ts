e65057faee3145bcbe95310be586c66d6348ecaa
BregNights <daniel_baumann567@hotmail.com>
2026-01-31 19:06:22 -0300

    add new unit test

--- src/domain/cine/application/use-cases/create-account.spec.ts
+++ src/domain/cine/application/use-cases/create-account.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
 import { CreateAccountUseCase } from './create-account'
+import { UserAlreadyExistsError } from './errors/user-already-exists.error'
 
 let inMemoryUsersRepository: InMemoryUsersRepository
 let sut: CreateAccountUseCase
@@ -17,5 +18,20 @@ describe('Create Account', () => {
     })
 
     expect(result.isRight()).toBe(true)
+  })
+
+  it('should not be able create a new account with same email', async () => {
+    await sut.execute({
+      name: 'example',
+      email: 'example@example.com',
+    })
+
+    const result = await sut.execute({
+      name: 'example1',
+      email: 'example@example.com',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(UserAlreadyExistsError)
   })
 })
